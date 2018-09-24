0745a635fe75a392c85a01ae89d097436d425a13
Samyak <smyk009@gmail.com>
2018-09-24 17:40:43 +0530

    PR2 : shopping-list, recipe-list implementation.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'TestRecipe',
+      'This is a test Recipe',
+      // tslint:disable-next-line:max-line-length
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'
+    ),
+    new Recipe(
+      'TestRecipe',
+      'This is a test Recipe',
+      // tslint:disable-next-line:max-line-length
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'
+    ),
+    new Recipe(
+      'TestRecipe',
+      'This is a test Recipe',
+      // tslint:disable-next-line:max-line-length
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'
+    )
+  ];
   constructor() { }
 
   ngOnInit() {
