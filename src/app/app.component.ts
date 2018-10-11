550bf967e52afe688090b5462f5f2a1f08192db2
Samyak <smyk009@gmail.com>
2018-10-11 19:33:07 +0530

    PR 3 : Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PROJECT';
-  itemNavigate = 'recipe';
+  itemNavigate = '';
   onNavigate(itemSelected: string) {
     this.itemNavigate = itemSelected;
   }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('inputName') nameInputRef: ElementRef;
+  @ViewChild('inputAmount') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const inpName = this.nameInputRef.nativeElement.value;
+    const inpAmt = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(inpName, inpAmt);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
